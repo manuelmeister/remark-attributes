96ada061a72e35a968babf3c9588bbd7b4dbfd80
Manuel Meister <news.manuelsworld@gmail.com>
2026-05-29 10:41:56 +0200

    v0.4.0

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
-import type {Root} from 'mdast'
 import type {Extension as FromMarkdownExtension} from 'mdast-util-from-markdown'
-import type {Processor, Transformer} from 'unified'
 import {micromarkAttributes} from './packages/micromark-attributes/index.js'
 import {mdastAttributes} from './packages/mdast-attributes/index.js'
 import {attributesTransformer} from './packages/attributes-transformer/index.js'
 import {AttributesExtension} from './util/types.js'
+
+export interface RemarkAttributesOptions {
+  mdx?: boolean
+}
 
 interface AttributesData {
   micromarkExtensions?: AttributesExtension[]
@@ -15,10 +17,11 @@ interface AttributesData {
  * Plugin to support attributes like markdown-it-attrs
  * [text](https://test.com){target=_blank}
  */
-export default function remarkAttributes(
-  this: Processor<Root, Root, Root, string>,
-  options = {mdx: false}
-): Transformer<Root> {
+function remarkAttributes(
+  this: any,
+  options: RemarkAttributesOptions = {}
+) {
+  const settings = {mdx: false, ...options}
   const data = this.data() as AttributesData
 
   function add<K extends keyof AttributesData>(
@@ -29,8 +32,10 @@ export default function remarkAttributes(
     data[key].unshift(value)
   }
 
-  add('micromarkExtensions', micromarkAttributes({escaped: options.mdx}))
+  add('micromarkExtensions', micromarkAttributes({escaped: settings.mdx}))
   add('fromMarkdownExtensions', mdastAttributes())
 
   return attributesTransformer
 }
+
+export default remarkAttributes as any
